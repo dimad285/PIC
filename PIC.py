c1d70284a4a74f6e6e1cc9d86d2ff9d006c7702f
dima285 <dimadudin285@gmail.com>
2024-10-14 20:22:22 +0200

    UI

--- PIC.py
+++ PIC.py
@@ -14,7 +14,10 @@ def run_gpu(m, n, X, Y, N, dt,
             solver = 'inverse', DIAG_TYPE = 'line', bins = 64):
     
     print('Starting...')
-    RUN = True
+    global RUN
+    global FINISH
+    FINISH = False
+    RUN = False
     framecounter = 0
     print('creating arrays...')
     gridsize = (m, n)
@@ -52,8 +55,27 @@ def run_gpu(m, n, X, Y, N, dt,
 
 
     if UI:
+        def toggle_simulation():
+            global RUN
+            RUN = not RUN
+            button.config(text="Stop" if RUN else "Start")
+
+
+        def close_window():
+            global RUN
+            global FINISH
+            RUN = False
+            FINISH = True
+            ui.destroy()
+
         ui = tk.Tk()
-        ui.mainloop()
+        ui.title("PIC Simulation Control")
+        button = tk.Button(ui, text="Start", command=toggle_simulation)
+        button.pack(pady=20)
+        ui.protocol("WM_DELETE_WINDOW", close_window)
+    
+    else:
+        RUN = True
     
     if RENDER:
         print('creating renderer...')
@@ -70,44 +92,61 @@ def run_gpu(m, n, X, Y, N, dt,
         phi = Solvers.solve_poisson_fft(rho, k_sq, Consts.eps0)
     Update.updateE_gpu(E, phi, X, Y, gridsize)
     Update.update_V(R, V, E, part_type, q_type, m_type_1, gridsize, -dt*0.5, X, Y)
-    
+
+    if RENDER:
+        renderer.update_particles(R, part_type)
+        renderer.render()
+
     print('running...')
-    while RUN:
-        start_time = time.time()
-        # UPDATE
-        R[:] += V[:] * dt
-        Update.update_density_gpu(R, part_type, rho, X, Y, gridsize, q_type)
-        if boundary != None:
-            rho[bound[0]] = bound[1]
-        if solver == 'inverse':
-            phi = cp.dot(Lap, -rho*Consts.eps0_1)
-        elif solver == 'fft':
-            phi = Solvers.solve_poisson_fft(rho, k_sq, Consts.eps0)
-        elif solver == 'cg':
-            phi = Solvers.solve_poisson_pcg_gpu(rho, m, n, dx, dy, Consts.eps0)
-        Update.updateE_gpu(E, phi, X, Y, gridsize)
-        Update.update_V(R, V, E, part_type, q_type, m_type_1, gridsize, dt, X, Y)
-
-        sim_time = time.time() - start_time
-        
-        #dt = min(1/cp.max(cp.abs(V[0]))/X/(m-1), 1/cp.max(cp.abs(V[1]))/Y/(n-1)) * 0.5
-        #print(dt)
-        # RENDER
-        framecounter += 1
-        if framecounter == RENDER_FRAME and RENDER:
-            renderer.update_particles(R, part_type)
-            renderer.render()
-            render_time = time.time() - start_time - sim_time
-            renderer.change_title(f"Frame time: {(time.time() - start_time)*1000:.2f} ms     Simulation time: {sim_time*1000:.2f} ms     Render time: {render_time*1000:.2f} ms   dt: {dt:.2e}")
-            if renderer.should_close():
+    while True:
+        if UI:
+            ui.update()
+            if FINISH:
                 break
+        
+        if RUN:
+            start_time = time.time()
+            # UPDATE
 
-            framecounter = 0
+            R[:] += V[:] * dt
+            Update.update_density_gpu(R, part_type, rho, X, Y, gridsize, q_type)
+            if boundary != None:
+                rho[bound[0]] = bound[1]
+            if solver == 'inverse':
+                phi = cp.dot(Lap, -rho*Consts.eps0_1)
+            elif solver == 'fft':
+                phi = Solvers.solve_poisson_fft(rho, k_sq, Consts.eps0)
+            elif solver == 'cg':
+                phi = Solvers.solve_poisson_pcg_gpu(rho, m, n, dx, dy, Consts.eps0)
+            Update.updateE_gpu(E, phi, X, Y, gridsize)
+            Update.update_V(R, V, E, part_type, q_type, m_type_1, gridsize, dt, X, Y)
+
+            sim_time = time.time() - start_time
+
+            # RENDER
+            framecounter += 1
+            if framecounter == RENDER_FRAME and RENDER:
+                renderer.update_particles(R, part_type)
+                renderer.render()
+                render_time = time.time() - start_time - sim_time
+                renderer.change_title(f"Frame time: {(time.time() - start_time)*1000:.2f} ms     Simulation time: {sim_time*1000:.2f} ms     Render time: {render_time*1000:.2f} ms   dt: {dt:.2e}")
+                
+                if renderer.should_close():
+                    break
+
+                framecounter = 0
 
         elif RENDER == False:
-            print(f"Frame time: {(time.time() - start_time)*1000:.2f}ms")
+            print(RUN)
 
-    renderer.close()
+        if not UI and not RENDER:
+            break
+
+    if RENDER:
+        renderer.close()
+        print('Renderer closed')
+    if UI and not FINISH:
+        ui.destroy()
 
     return 0
 
